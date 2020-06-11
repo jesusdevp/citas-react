6fe7ea190f7513f5e252c66f33caf09f84ae139c
Jesus Ramirez <jesusdevelp@gmail.com>
2020-06-10 19:18:02 -0500

    Se agrego componente Cita

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from "react";
 import Formulario from "./components/Formulario";
+import Cita from "./components/Cita";
 
 function App() {
   //Arreglo de citas
@@ -11,6 +12,16 @@ function App() {
     guardarCitas([...citas, cita]);
   };
 
+  //Funcion que elimina cita
+
+  const eliminarCita = (id) => {
+    const nuevasCitas = citas.filter((cita) => cita.id !== id);
+    guardarCitas(nuevasCitas);
+  };
+
+  //Mensaje condicional
+  const titulo = citas.length === 0 ? "No hay citas" : "Administra tus citas";
+
   return (
     <Fragment>
       <h1>Administrador de Pacientes</h1>
@@ -19,7 +30,12 @@ function App() {
           <div className="one-half column">
             <Formulario crearCita={crearCita} />
           </div>
-          <div className="one-half column">2</div>
+          <div className="one-half column">
+            <h2>{titulo}</h2>
+            {citas.map((cita) => (
+              <Cita key={cita.id} cita={cita} eliminarCita={eliminarCita} />
+            ))}
+          </div>
         </div>
       </div>
     </Fragment>
